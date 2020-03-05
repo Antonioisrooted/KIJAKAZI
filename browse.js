36ecc678d6f7a83a6fac4ab20bbdc87d4da57d2f
mamokunni <makuel.mk@gmail.com>
2020-03-05 14:02:59 +0300

    add all files and updates

--- browse.js
+++ browse.js
@@ -1,10 +1,24 @@
 
 $(document).ready(function(){
 
+    
+    $('#makebid').on ('click', function() {
+        $('.browseform').show();
+        $('.map').hide();
+      });
 
-  $('#makebid').on ('click', function() {
-    $('.browseform').show();
-    $('.map').hide();
-  });
 });
 
+//constructor to collect information from the forms.
+//function dataCollector(/*all form variables go in here*/){
+  //this.formvariable=formvariable;
+  //this.allthevariableslikewise=allthevariableslikewise;
+//}
+
+//section for getting values from the forms
+
+//an instance of our constructor class to place the information gotten from the form to the specific view points
+//var displaycards = new dataCollector(allTheVariablesInTheDataCollectorConstructor);
+//callfunctionstodisplaysth(again, puttheVariablesResponsibleInHere,justTheName);
+//the functions may be more than one so put all the necessary functions
+//$(".idOfTheSectionYouWantInfoDisplayed").text(idOfTheFormSectionYouAreTargetting);
