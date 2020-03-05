59f2474ed373e2aad4ccbde794f1deda512e4b9b
mamokunni <makuel.mk@gmail.com>
2020-03-05 15:05:43 +0300

    add all files and updates

--- js/browse.js
+++ js/browse.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  $('#makebid1').on ('click', function(e){
+    e.preventDefault();
+    alert('Success. Your Bid has been Placed');
+  });
+
+  $('#view').on ('click', function() {
+    $('.job_detail').toggle();
+    });
   $('#makebid').on ('click', function() {
     $('.browseform').toggle();
     $('.map').toggle();
